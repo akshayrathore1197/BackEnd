67c64f8b521e14cdb8ff78a16d8f6a93d88d9257
Akshay Rathore <akshayrathore2024@gmail.com>
2024-09-27 12:56:03 +0530

    Now I Know Routes

---
+++ 05_VidTube-Project/src/controllers/healthcheck.controllers.js
@@ -0,0 +1,10 @@
+import { APiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new APiResponse(200, "OK", "Health check passed"));
+});
+ 
+export { healthcheck };
